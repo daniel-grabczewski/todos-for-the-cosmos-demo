f69b31999b5fb3f0ac139b7844e3e2ef64da45d3
Daniel Grabczewski <danielgrabczewski@gmail.com>
2023-10-04 11:49:16 +1300

    modify todolist core styling

--- client/components/TodoApp.tsx
+++ client/components/TodoApp.tsx
@@ -4,7 +4,8 @@ import List from './List'
 const TodoApp = () => {
   return (
     <div style={{
-      background: 'white',
+      background: 'rgba(0, 0, 0, 0.67)',
+      color: '#F1F1F1',
       position: 'fixed',
       top: '50%',
       left: '50%',
