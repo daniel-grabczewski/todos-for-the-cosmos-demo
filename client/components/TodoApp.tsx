feb079ea03b67d4510177b0117f2d74c79e456f2
Daniel Grabczewski <danielgrabczewski@gmail.com>
2023-10-08 09:58:52 +1300

    make todo interface borders rounded

--- client/components/TodoApp.tsx
+++ client/components/TodoApp.tsx
@@ -3,14 +3,18 @@ import List from './List'
 
 const TodoApp = () => {
   return (
-    <div style={{
-      background: 'rgba(0, 0, 0, 0.67)',
-      color: '#F1F1F1',
-      position: 'fixed',
-      top: '50%',
-      left: '50%',
-      transform: 'translate(-50%, -50%)'
-    }}>
+    <div
+      style={{
+        background: 'rgba(0, 0, 0, 0.55)',
+        color: '#F1F1F1',
+        position: 'fixed',
+        top: '50%',
+        left: '50%',
+        transform: 'translate(-50%, -50%)',
+        borderRadius: '10px',
+        padding: '20px 20px'
+      }}
+    >
       <AddTodo />
       <List />
     </div>
