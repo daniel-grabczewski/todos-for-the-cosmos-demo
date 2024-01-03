2b338225f35d55d697834b18f32e826a00ba3525
Daniel Grabczewski <danielgrabczewski@gmail.com>
2024-01-03 13:00:01 +1300

    create initial data file

---
+++ client/data/data.js
@@ -0,0 +1,5 @@
+export const initialTodos = [
+  { id: 1, todo: 'Start a civilization on Mars', priority: 2, isCompleted: false },
+  { id: 2, todo: 'Explore a new galaxy', priority: 4, isCompleted: true },
+  { id: 3, todo: 'Walk the surface of the sun', priority: 5, isCompleted: false },
+];
