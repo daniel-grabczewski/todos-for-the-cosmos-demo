9636f8f60d6993df0f9e966a55221b2bf3fb12ad
Daniel Grabczewski <danielgrabczewski@gmail.com>
2024-01-06 12:24:56 +1300

    deploy app

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  base: '/todos-for-the-cosmos-demo/',
   server: {
     proxy: {
       '/api': 'http://localhost:3000',
